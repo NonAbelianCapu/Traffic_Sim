3af16632f7f2e49764fb0259595371441e3e5451
NonAbelianCapu <acaputobugallo@gmail.com>
2021-09-17 12:25:19 -0300

    add some utils, plotting and flow estimation

--- discrete_model/main.py
+++ discrete_model/main.py
@@ -1,15 +1,26 @@
-from sim import *
+import sim
+import utils
+import numpy as np
+import matplotlib.pyplot as plt
 
-pos = np.zeros(40)
-vel = np.zeros(40)
-N=40
-L=250
 
-populate_arrays(pos,vel,N)
-pos_list = run_simulation(pos,vel,N,L)
+def main():
 
-plt.imshow(pos_list, cmap='inferno')
-plt.show()
+
+    N=40
+    L=250
+
+    pos = np.zeros(N)
+    vel = np.zeros(N)
+
+    sim.populate_arrays(pos,vel,N)
+    pos_list = sim.run_simulation(pos,vel,N,L, MAX_STEPS=250, p = 0)
+    flow = utils.estimate_flow(pos_list,N, 0,250)
+    sim_fig = utils.plot_simulation(pos_list)
+    plt.show()
+    plt.plot(flow)
+    plt.show()
+
 
 if __name__ == '__main__':
     main()
---
+++ discrete_model/utils.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def estimate_flow(pos_grid,N,i,f):
+
+    # estimates flow over time between position i and f in the road
+
+    flow = []
+    L = len(pos_grid[0])
+    T = len(pos_grid)
+
+    assert (i >= 0 and i <= L-1), "i out of bound"
+    assert (f >= 0 and f <= L-1), "f out of bound"
+
+    for row in pos_grid:
+        temp_array = row[i:f]
+        temp_flow = np.sum(temp_array)*(N/(L*T))
+        flow.append(temp_flow)
+
+    return flow
+
+
+
+def vel_spec(pos_grid):
+    pass
+
+
+def plot_simulation(pos_grid):
+
+    fig = plt.figure(figsize=(10,6))
+
+    # we add some contrast to the velocity to the background of imshow
+
+    for r,row in enumerate(pos_grid):
+        for c,col in enumerate(row):
+            pos_grid[r][c] += 1
+
+    plt.xlabel('Occupation', fontsize = 13)
+    plt.ylabel('Time', fontsize = 13)
+    plt.title('Simulation')
+    plt.legend()
+    plt.imshow(pos_grid, cmap='inferno')
+
+    return fig
